6ba5b5e4182b19b2005d0f949c80e18d0ae64ea4
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2017-10-17 23:28:45 +0200

    Add constructor helper methods

--- TSP/GraphRepresentation.cpp
+++ TSP/GraphRepresentation.cpp
@@ -1,9 +1,10 @@
 #include "stdafx.h"
 #include "GraphRepresentation.h"
+#include <math.h>
 
 using namespace TSP;
 
-weight ** TSP::GraphRepresentation::AllocateSquareMatrix(uint matrix_size)
+weight ** GraphRepresentation::AllocateSquareMatrix(uint matrix_size)
 {
 	weight **matrix = new weight*[matrix_size];
 	for (uint i = 0; i < matrix_size; i++)
@@ -15,3 +16,24 @@ weight ** TSP::GraphRepresentation::AllocateSquareMatrix(uint matrix_size)
 	return matrix;
 }
 
+void GraphRepresentation::ConvertCoordinatesToWeights()
+{
+	uint destination_vertex = 1;
+	for (uint i = 0; i < coordinates_.size() - 1; i++)
+	{
+		for (uint j = destination_vertex; j < coordinates_.size(); j++)
+		{
+			edges_num_++;
+			weight edge_weight = GetEuclideanDistance(coordinates_[i], coordinates_[j]);
+			AddEdge(i, j, edge_weight);
+		}
+		destination_vertex++;
+	}
+}
+
+weight GraphRepresentation::GetEuclideanDistance(Vertex source, Vertex destination)
+{
+	int X = source.x_ - destination.x_;
+	int Y = source.y_ - destination.y_;
+	return (sqrt(pow(X, 2) + pow(Y, 2)));
+}
