b390ec3c32666d9b718f1f21a8d42e930295c814
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2017-10-26 22:35:08 +0200

    Add TSP_API export to class

--- TSP/Solution.cpp
+++ TSP/Solution.cpp
@@ -7,6 +7,17 @@ Solution::Solution()
 {
 }
 
+TSP::Solution::Solution(int distance)
+{
+	distance_ = distance;
+}
+
+TSP::Solution::Solution(int distance, std::vector<int> path)
+{
+	distance_ = distance;
+	path_ = path;
+}
+
 
 Solution::~Solution()
 {
--- TSP/Solution.h
+++ TSP/Solution.h
@@ -1,13 +1,25 @@
 #pragma once
 
+#include <vector>
+
+#ifdef TSP_EXPORTS
+#define TSP_API __declspec(dllexport)
+#else
+#define TSP_API __declspec(dllimport)
+#endif
 
 namespace TSP
 {
-	class Solution
+	class TSP_API Solution
 	{
 	public:
 		Solution();
+		Solution(int distance);
+		Solution(int distance, std::vector<int> path);
 		~Solution();
+
+		int distance_;
+		std::vector<int> path_;
 
 	};
 }
