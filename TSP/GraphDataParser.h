381d7b05dfc2ad4bedabca60660bc12402a8e938
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2017-11-10 01:37:59 +0100

    Restructure project

--- TSP/GraphDataParser.h
+++ TSP/GraphDataParser.h
@@ -2,13 +2,16 @@
 
 #include <fstream>
 #include <type_traits>
-#include "GraphRepresentation.h"
+#include "Graphs\GraphRepresentation.h"
+#include <iterator>
+#include <limits>
+#include <sstream>
 
 
 namespace TSP
 {
-	template<typename R>
-	R ParseGraphFile(std::string filepath);	
+	template<typename Graph, typename Cost>
+	Graph ParseGraphFile(std::string filepath);	
 
 	inline std::vector<Vertex> ReadCoordinates(std::fstream & stream)
 	{
@@ -19,8 +22,16 @@ namespace TSP
 		return coordinates;
 	}
 
-	template<typename R>
-	inline R  ParseGraphFile(std::string filepath)
+
+	template <typename Cost>
+	inline std::vector<std::vector<Cost>> ReadMatrix(std::fstream & stream);
+
+	template <typename Cost>
+	inline std::vector<std::vector<Cost>> ReadFullMatrix(std::fstream & stream);
+	
+	
+	template<typename Graph, typename Cost>
+	inline Graph ParseGraphFile(std::string filepath)
 	{
 
 		std::fstream file;
@@ -31,13 +42,23 @@ namespace TSP
 
 		std::string file_type;
 		std::getline(file, file_type);
-		R rep;
+		Graph rep;
 
 
 		if (file_type == "NODE_COORD")
 		{
 			std::vector<Vertex> coordinates = ReadCoordinates(file);
-			rep = R(coordinates);
+			rep = Graph(coordinates);
+		}
+		else if (file_type == "MATRIX")
+		{
+			auto mat = ReadMatrix<Cost>(file);
+			rep = Graph(mat);
+		}
+		else if (file_type == "FULL_MATRIX")
+		{
+			auto mat = ReadFullMatrix<Cost>(file);
+			rep = Graph(mat);
 		}
 
 		/*else if (file_type == "EDGE_WEIGHT")	
@@ -48,5 +69,44 @@ namespace TSP
 		file.close();
 		return rep;
 	}
+
+	template<typename Cost>
+	inline std::vector<std::vector<Cost>> ReadMatrix(std::fstream & stream)
+	{
+		std::string line;
+		auto vec = std::vector<std::vector<Cost>>();
+		while (std::getline(stream, line))
+		{
+			std::istringstream is(line);
+			vec.push_back(std::vector<int>(std::istream_iterator<int>(is), std::istream_iterator<int>()));
+		}
+		for (uint i = 0; i < vec.size(); i++)
+			vec[i][i] = std::numeric_limits<Cost>::max();
+
+		return vec;
+	}
+
+	template<typename Cost>
+	std::vector<std::vector<Cost>> ReadFullMatrix(std::fstream & stream)
+	{	
+		// Read all the numbers into a vector
+		auto raw_vec = std::vector<Cost>(std::istream_iterator<Cost>(stream), std::istream_iterator<Cost>());
+		uint chunk = (uint)sqrt(raw_vec.size());
+		uint start = 0;
+		auto vec = std::vector<std::vector<Cost>>();
+		for (uint i = 0; i < chunk; i++)
+		{
+			vec.push_back(std::vector<Cost>(raw_vec.begin() + start, raw_vec.begin() + start + chunk));
+			start += chunk;
+		}	
+
+		for (uint i = 0; i < vec.size(); i++)
+			vec[i][i] = std::numeric_limits<Cost>::max();
+
+		return vec;
+	}
+
+	
+	
 }
 
--- TSP/TSP.cpp
+++ TSP/TSP.cpp
@@ -4,10 +4,95 @@
 #include "stdafx.h"
 #include "AsymmetricAdjacencyMatrix.h"
 #include "SymmetricAdjacencyMatrix.h"
-#include "BB.h"
-
+#include "GraphDataParser.h"
+#include "LittleMatrix.h"
+#include "BranchAndBound.h"
+#include "PerformanceTests.h"
+#include <fstream>
+#include <conio.h>
 using namespace TSP;
+
+using std::cout;
+using std::endl;
+using std::string;
+
+void StartMenu()
+{
+	Solution<int> result;
+	double time = 0.0;
+	uint visited_nodes = 0;
+	char option = '1';
+	do
+	{
+		cout << "\n--Menu--" << endl;
+		cout << "Travelling salesman problem using Branch and Bound method" << endl;
+		cout << "1. Solve symmetric instance" << endl;
+		cout << "2. Solve asymmetric instance" << endl;
+		cout << "3. Show last resul" << endl;
+		cout << "O. Exit" << endl;
+
+		option = _getche();
+	
+		string filepath;
+		switch (option)
+		{
+			case '1':
+			{
+				cout <<"\nEnter the relative filepath: " << endl;
+				std::getline(std::cin, filepath);
+				if (FileExists(filepath))
+				{
+					auto graph = ParseGraphFile<SymmetricAdjacencyMatrix<int>, int>(filepath);
+					auto solver = BranchAndBound<int>();				
+					StartCounter();
+					result = solver.Solve(graph);
+					time = GetCounter();
+					visited_nodes = solver.VisitedNodesCount();
+					cout << "\nTime of solving: " << time << " ms. Visited " << visited_nodes << " nodes." << endl;
+					cout << "The optimal tour: " << endl;
+					result.PrintTour();
+					cout << "The total tour distance: " << result.distance_;
+				}
+			}break;
+			
+			case '2':
+			{
+				cout << "\nEnter the relative filepath: \n" << endl;
+				std::getline(std::cin, filepath);
+				if (FileExists(filepath))
+				{
+					auto graph = ParseGraphFile<AsymmetricAdjacencyMatrix<int>, int>(filepath);
+					auto solver = BranchAndBound<int>();			
+					StartCounter();
+					result = solver.Solve(graph);
+					time = GetCounter();
+					visited_nodes = solver.VisitedNodesCount();
+					cout << "\nTime of solving: " << time << " ms. Visited " << visited_nodes << " nodes." << endl;
+					cout << "The optimal tour: " << endl;
+					result.PrintTour();
+					cout << "The total tour distance: " << result.distance_ << endl;
+				}
+			}break;
+			case '3':
+			{	
+				cout << "\nTime of solving: " << time << " ms. Visited " << visited_nodes << " nodes." << endl;
+				cout << "The optimal tour: " << endl;
+				result.PrintTour();
+				cout << "The total tour distance: " << result.distance_ << endl;
+				
+			}break;
+			case '0':
+				break;
+			default:
+				break;
+		}
+	} while (option != 0);
+}
+
 int main()
 {
+	StartMenu();
+	//RunTests(100);
 }
+
 
