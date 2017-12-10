4d787080080290f76861f4d5e768e482043e37ca
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2017-12-10 18:17:52 +0100

    Add class to represent moves that generate solution neighbourhood in tabu search

--- TSP/Solvers/TabuSearch/Move.h
+++ TSP/Solvers/TabuSearch/Move.h
@@ -5,10 +5,10 @@
 
 #include "..\Solver.h"
 #include "..\..\Graphs\GraphRepresentation.h"
+#include "TabuList.h"
 
 namespace TSP
 {
-	using MoveParameters = std::vector<uint>;
 
 	// The method of moving from one solution to another
 	template<typename Cost>
@@ -17,7 +17,8 @@ namespace TSP
 	public:
 		Move() {};
 		~Move() {};
-		virtual void Execute(Solution<Cost> &solution, const MoveParameters &parameters, const GraphRepresentation<Cost> &representation) = 0;
+		virtual Solution<Cost> Execute(Solution<Cost> &solution, MoveParameters &parameters,  GraphRepresentation<Cost> &representation) = 0;
+		virtual Cost EvaluateMove(Solution<Cost> &solution, MoveParameters &parameters, GraphRepresentation<Cost> &representation) = 0;
 	};
 
 
@@ -25,20 +26,65 @@ namespace TSP
 	class Swap
 		: public Move<Cost>
 	{
+	public:
 		Swap() {};
 		~Swap() {};
-		void Execute(Solution<int> &solution, const MoveParameters &parameters, const GraphRepresentation<Cost> &representation);
+	    virtual Solution<Cost> Execute(Solution<Cost> &solution, MoveParameters &parameters, GraphRepresentation<Cost> &representation) override;
+		virtual Cost EvaluateMove(Solution<Cost> &solution, MoveParameters &parameters, GraphRepresentation<Cost> &representation) override;
+
 	};
 
 
-	// Change the solution by swapping 2 vertives in tour
+	// Swaps the vertives described by parameters.
+	// In this case, parameters describe the indices of vertices relative to
+	// their position in tour, and not their indices in graph
 	template<typename Cost>
-	inline void Swap<Cost>::Execute(Solution<int>& solution, const MoveParameters &parameters, const GraphRepresentation<Cost>& representation)
+	inline Solution<Cost> Swap<Cost>::Execute(Solution<Cost>& solution, MoveParameters & parameters, GraphRepresentation<Cost>& representation)
 	{
-		auto temp = solution.tour[parameters[0]];
-		solution.tour[0] = solution.tour[parameters[1]];
-		solution.tour[1] = temp;		
-		solution.total_cost = representation.GetTourCost(solution.tour);
+		//Cost total_cost = EvaluateMove(solution, parameters, representation);
+		auto tour = solution.tour;
+		auto temp = tour[parameters[0]];
+		tour[parameters[0]] = tour[parameters[1]];
+		tour[parameters[1]] = temp;
+		Cost total_cost = representation.GetTourCost(tour);		
+		return Solution<Cost>(total_cost, tour);
+	}
+
+	// Calculates total_cost of new solution in constant time
+	template<typename Cost>
+	inline Cost Swap<Cost>::EvaluateMove(Solution<Cost>& solution, MoveParameters & params, GraphRepresentation<Cost>& representation)
+	{
+		Cost new_cost = solution.total_cost;
+		// Subtract the cost of edges that were removed due to the swap
+		// In tour : 1-2-3-4-5-6-1 if we're to swap vertices 2 and 5, to get the new cost 
+		// of tour : 1-5-3-4-2-6-1 we need to subtract the cost of edges 1-2,2-3 and 4-5 and 5-6 from the old tour
+		// and then add cost of edges 1-5,5-3,4-2,6-1
+		// In the case above, the MoveParameters would be (1,4)
+		// Need to remove (vertex_before - tour[params[0]-1], tour[parms[0]]), (vertex_before - tour[params[0]-1], tour[parms[0]]) same for params[1]
+		// As the indices in graph representation are numered from 0, subtract 1 from the above results
+
+		// Get which vertices this is really about
+		int left, left_prev, left_next, right, right_prev, right_next;
+
+		left = solution.tour[params[0]] - 1;
+		left_prev = solution.tour[params[0] - 1] - 1;
+		left_next = solution.tour[params[0] + 1] - 1;
+
+		right = solution.tour[params[1]] - 1;
+		right_prev = solution.tour[params[1] - 1] - 1;
+		right_next = solution.tour[params[1] + 1] - 1;
+
+		new_cost -= representation.GetWeight(left_prev, left);
+		new_cost -= representation.GetWeight(left, left_next);
+		new_cost -= representation.GetWeight(right_prev, right);
+		new_cost -= representation.GetWeight(right, right_next);
+
+		new_cost += representation.GetWeight(left_prev, right);
+		new_cost += representation.GetWeight(right, left_next);
+		new_cost += representation.GetWeight(right_prev, left);
+		new_cost += representation.GetWeight(left, right_next);		
+
+		return new_cost;
 	}
 
 }
