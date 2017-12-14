71dc382c0dd56c0936e020a4e472ec37058ba0fc
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2017-12-14 19:26:22 +0100

    Add tests for instances of size 280 and 575

--- TSP.Tests/TabuSearchTests.cpp
+++ TSP.Tests/TabuSearchTests.cpp
@@ -118,5 +118,41 @@ namespace TSPTests
 			Assert::AreEqual(true, IsAccurate(2085, actual.total_cost, 0.9), message.c_str());
 		}
 
+		TEST_METHOD(TabuSearchSym280)
+		{
+			auto params = TabuParameters();
+			params.max_iterations = 5000;
+			params.tabu_list_length = 20;
+			params.tabu_tenure = 20;
+			params.max_no_improve = 500;
+			params.restart_count = 3;
+			params.max_time_s = 20 * 60;
+
+			auto graph = ParseGraphFile<SymmetricAdjacencyMatrix<int>, int>("C:\\Users\\jakub\\Documents\\Visual Studio 2017\\Projects\\PEA\\TSP\\Benchmarks\\ProblemData\\TSPLIB\\Symmetric\\a280.txt");
+			auto solver = TabuSearch<int>(params);
+			auto move = new Swap<int>();
+			auto actual = solver.Solve(graph);
+			std::wstring message = L"Optimal: 2579, actual: " + std::to_wstring(actual.total_cost);
+			Assert::AreEqual(true, IsAccurate(2579, actual.total_cost, 0.9), message.c_str());
+		}
+
+		TEST_METHOD(TabuSearchSym575)
+		{
+			auto params = TabuParameters();
+			params.max_iterations = 5000;
+			params.tabu_list_length = 20;
+			params.tabu_tenure = 15;
+			params.max_no_improve = 500;
+			params.restart_count = 3;
+			params.max_time_s = 10 * 60;
+
+			auto graph = ParseGraphFile<SymmetricAdjacencyMatrix<int>, int>("C:\\Users\\jakub\\Documents\\Visual Studio 2017\\Projects\\PEA\\TSP\\Benchmarks\\ProblemData\\TSPLIB\\Symmetric\\rat575.txt");
+			auto solver = TabuSearch<int>(params);
+			auto move = new Swap<int>();
+			auto actual = solver.Solve(graph);
+			std::wstring message = L"Optimal: 6773, actual: " + std::to_wstring(actual.total_cost);
+			Assert::AreEqual(true, IsAccurate(6773, actual.total_cost, 0.9), message.c_str());
+		}
+
 	};
 }
