0c188abbb3b4350ea2fc2ebc973a134b1cddcfdd
Jakub Sokolowski <jakub.sokolowski1996@gmail.com>
2019-01-21 00:22:30 +0100

    Change Population's underlying data structor to Multiset

--- TSP/Solvers/Genetic/Population.h
+++ TSP/Solvers/Genetic/Population.h
@@ -11,10 +11,15 @@
 #include <algorithm>
 #include <numeric>
 #include <random>
+#include <set>
 #include "RandomHelper.h"
 
 namespace TSP
 {
+	enum Selection {
+		ROULETTE,
+		TOURNAMENT
+	};
 	// Represents the population of avalible chromosomes and handles random generation and selection
 	template<typename Cost>
 	class Population
@@ -25,24 +30,33 @@ namespace TSP
 		Population(int population_size);
 		Population(int population_size, GraphRepresentation<Cost>& representation);
 		Population(const Population<Cost> &other);
-		Population(const std::vector<Chromosome<Cost>> &chromosomes);
+		Population(const std::multiset<Chromosome<Cost>> &chromosomes);
+		Population(const Population<Cost> &other, double elite_percentage);
 		Population<Cost> operator = (const Population& other);
+
 		~Population();
+	
+		int GetSize() const { return population_m.size(); }
+		int GetMaxSize() const { return max_pop_size_m; }
+		bool IsFull() { return GetSize() == GetMaxSize(); }
 
 		void AddChromosome(Chromosome<Cost> &chromosome);
 
-		int GetSize() const { return population_m.size(); }
-		int GetMaxSize() const { return max_pop_size_m; }
-		Chromosome<Cost> GetMostFit() const;
+	
 		Cost GetAvergeTourCost() const { return  Cost(std::accumulate(population_m.) / population_m.size()) }
-		Chromosome<Cost> GetBestChromosomeWithProbability(double probability, std::vector<Chromosome<Cost>> &chromosomes);
-		std::vector<Chromosome<Cost>> GetKChromosomes(int k);
+		Chromosome<Cost> GetMostFit() const;
+		std::vector<Chromosome<Cost>> GetMostFitPercentage(double percentage) const;	
+		Chromosome<Cost> GetBestChromosomeWithProbability(double probability, std::multiset<Chromosome<Cost>> &chromosomes);
+		std::multiset<Chromosome<Cost>> GetKChromosomes(int k);
 		Chromosome<Cost> TournamentSelection(int tournament_size);
+		Chromosome<Cost> RouletteSelection();
+		std::pair<Chromosome<Cost>, Chromosome<Cost>> GetParents(Selection sel);
 
 	private:
 
 		int max_pop_size_m;
-		std::vector<Chromosome<Cost>> population_m;
+		int tournament_size = 10;
+		std::multiset<Chromosome<Cost>> population_m;
 		void GenerateRandomPopulation(GraphRepresentation<Cost> &representation);
 		Chromosome<Cost> CreateRandomChromosome(GraphRepresentation<Cost> &representation);
 		
@@ -68,7 +82,7 @@ namespace TSP
 		max_pop_size_m = other.max_pop_size_m;
 	}
 	template<typename Cost>
-	inline Population<Cost>::Population(const std::vector<Chromosome<Cost>>& chromosomes)
+	inline Population<Cost>::Population(const std::multiset<Chromosome<Cost>>& chromosomes)
 		: population_m(chromosomes)
 		, max_pop_size_m(chromosomes.size())
 	{}
@@ -80,6 +94,13 @@ namespace TSP
 		return *this;
 	}
 	template<typename Cost>
+	inline Population<Cost>::Population(const Population<Cost>& other, double elite_percentage) {
+		auto elite = other.GetMostFitPercentage(elite_percentage);
+		for (auto el : elite)
+			population_m.insert(el);
+		max_pop_size_m = other.max_pop_size_m;
+	}
+	template<typename Cost>
 	inline Population<Cost>::~Population()
 	{
 	}
@@ -87,20 +108,33 @@ namespace TSP
 	inline void Population<Cost>::AddChromosome(Chromosome<Cost>& chromosome)
 	{
 		if (GetSize() < max_pop_size_m)
-			population_m.push_back(chromosome);
+			population_m.insert(chromosome);
 	}
 
 	// Returns the iterator to the most fit chromosome - the one with the highest 1 / tour_cost ratio
 	template<typename Cost>
 	inline Chromosome<Cost> Population<Cost>::GetMostFit() const
 	{
-		return *std::min_element(population_m.begin(), population_m.end(), ChromosomeCompare<Cost>());
+		return *population_m.rbegin();
+	}
+	template<typename Cost>
+	inline std::vector<Chromosome<Cost>> Population<Cost>::GetMostFitPercentage(double percentage) const {
+		int n = std::round(percentage * population_m.size());
+		auto result = std::vector<Chromosome<Cost>>();
+		int counter = 0;
+		for (auto it = population_m.rbegin(); it != population_m.rend(); ++it) {
+			result.push_back(*it);
+			counter++;
+			if (counter == n)
+				break;
+		}
+		return result;
 	}
 	template<typename Cost>
 	inline void Population<Cost>::GenerateRandomPopulation(GraphRepresentation<Cost>& representation)
 	{
-		for (int i = 0; i < max_pop_size_m; i++)		
-			population_m.push_back(CreateRandomChromosome(representation));		
+		for (int i = 0; i < max_pop_size_m; i++)
+			population_m.insert(CreateRandomChromosome(representation));		
 	}
 	// Creates random chromosome for given graph. In tsp chromosome is combination of graph tour and the cost of this tour
 	template<typename Cost>
@@ -113,25 +147,64 @@ namespace TSP
 		std::random_shuffle(std::next(tour.begin(), 1), std::prev(tour.end(), 1));
 		return Chromosome<Cost>(representation.GetTourCost(tour), tour);
 	}
-	// Returns vector of K randomly selected chromosomes from population
+	// Returns multiset of K randomly selected chromosomes from population
 	template<typename Cost>
-	inline std::vector<Chromosome<Cost>> Population<Cost>::GetKChromosomes(int k)
+	inline std::multiset<Chromosome<Cost>> Population<Cost>::GetKChromosomes(int k)
 	{
-		std::vector<Chromosome<Cost>> chromosomes;
+		std::multiset<Chromosome<Cost>> chromosomes;
 		for (int i = 0; i < k; i++)		
-			chromosomes.push_back(*RandomHelper::RandomElement(population_m.begin(), population_m.end()));
+			chromosomes.insert(*RandomHelper::RandomElement(population_m.begin(), population_m.end()));
 		return chromosomes;
 	}
 	// Returns the best chromosome with specified probability. Otherwise, returns random chromosome
 	template<typename Cost>
-	inline Chromosome<Cost> Population<Cost>::GetBestChromosomeWithProbability(double probability, std::vector<Chromosome<Cost>> &chromosomes)
+	inline Chromosome<Cost> Population<Cost>::GetBestChromosomeWithProbability(double probability, std::multiset<Chromosome<Cost>> &chromosomes)
 	{
-		return RandomHelper::EventOccured(probability) ? *RandomHelper::RandomElement(chromosomes.begin(), chromosomes.end()) : GetMostFit();
+		return RandomHelper::EventOccured(probability) ? GetMostFit() : *RandomHelper::RandomElement(chromosomes.begin(), chromosomes.end());
 	}
 	template<typename Cost>
 	inline Chromosome<Cost> Population<Cost>::TournamentSelection(int tournament_size)
 	{
 		return GetBestChromosomeWithProbability(0.9, GetKChromosomes(tournament_size));
+	}
+	template<typename Cost>
+	inline Chromosome<Cost> Population<Cost>::RouletteSelection() {
+		// calculate the total weight
+		double weight_sum = 0;
+		for (auto ch : population_m) {
+			weight_sum += ch.GetFitness();
+		}
+		// get a random value
+		double value = RandomHelper::GetRandomInRange(0.0, 1.0) * weight_sum;
+		// locate the random value based on the weights
+		for (auto ch : population_m) {
+			value -= ch.GetFitness();
+			if (value < 0)
+				return ch;
+		}
+		// when rounding errors occur, we return the last item's index 
+		return *population_m.begin();
+	}
+	template<typename Cost>
+	inline std::pair<Chromosome<Cost>, Chromosome<Cost>>  Population<Cost>::GetParents(Selection sel) {
+		switch (sel)
+		{
+		case TSP::ROULETTE:
+			auto first_parent = RouletteSelection();
+			auto second_parent = RouletteSelection();
+			while (first_parent == second_parent)
+				second_parent = RouletteSelection();
+			return std::make_pair(first_parent, second_parent);
+		case TSP::TOURNAMENT:
+			auto first_parent = TournamentSelection(10);
+			auto second_parent = TournamentSelection(10);
+			while (first_parent == second_parent)
+				second_parent = TournamentSelection(10);
+			return std::make_pair(first_parent, second_parent);
+		default:
+			break;
+		}
+		return Chromosome<Cost>();
 	}
 }
 #endif // !TSP_POPULATION
